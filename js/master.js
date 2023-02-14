196aa14ca35519be6dc6e378bfc6270e527b038a
vargata <vargatam@yahoo.com>
2023-02-14 15:05:58 +0000

    cookie fix attempt 4

--- js/master.js
+++ js/master.js
@@ -42,6 +42,6 @@ function getCookies() {
 
 function setCookies(){
 	let now = new Date();
-	document.cookie = "app=JS-Array; expires=" +  now.setDate(now.getDate() + 30).toUTCString() + "; path=/";
-	document.cookie = "emails=" + lstEmail.toString() + "; expires=" +  now.setDate(now.getDate() + 30).toUTCString() + "; path=/";
+	document.cookie = "app=JS-Array; expires=" +  new Date(now.setDate(now.getDate() + 30)).toUTCString() + "; path=/";
+	document.cookie = "emails=" + lstEmail.toString() + "; expires=" +  new Date(now.setDate(now.getDate() + 30)).toUTCString() + "; path=/";
 }
